ed4185cea946b85a5141c7e2605e9f1ae3734f25
ZlabiDev <zlabi.dev@gmail.com>
2023-08-26 08:17:04 +0200

    add dockerization, fixed compilation, release 0.1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {
-    ZdStaticRouteDataInRouterOutletComponent
-} from './components/zd-static-route-data/zd-static-route-data-in-router-outlet/zd-static-route-data-in-router-outlet.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        ZdStaticRouteDataInRouterOutletComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
